1f1910d7be1589020551b36e9bd3a36c357f3043
phuslee <115574308+phuslee24@users.noreply.github.com>
2024-05-25 23:48:16 +0700

    CRUD JobType (#4)
    * CRUD JobType
    
    * Update Config.java
    
    ---------
    
    Co-authored-by: BlinhNhi <trantoan1682016@gmail.com>

---
+++ src/main/java/com/company/phtv/Services/JobTypeService.java
@@ -0,0 +1,68 @@
+package com.company.phtv.Services;
+
+import com.company.phtv.Models.DTO.IndustryDTO;
+import com.company.phtv.Models.DTO.JobTypeDTO;
+import com.company.phtv.Models.Entity.Industry;
+import com.company.phtv.Models.Entity.JobType;
+import com.company.phtv.Models.Map.IndustryMapping;
+import com.company.phtv.Models.Map.JobTypeMapping;
+import com.company.phtv.Models.Request.RequestJobType;
+import com.company.phtv.Repository.IndustryRepo;
+import com.company.phtv.Repository.JobRepo;
+import com.company.phtv.Repository.JobTypeRepo;
+import com.company.phtv.Services.IServices.IJobTypeService;
+import com.company.phtv.Utils.HttpException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class JobTypeService implements IJobTypeService {
+    @Autowired
+    JobTypeRepo _jobTypeRepo;
+
+
+    @Override
+    public List<JobTypeDTO> GetAll() {
+        List<JobType> jobTypes = _jobTypeRepo.findAll();
+        List<JobTypeDTO> jobTypeDTOS = new ArrayList<>();
+        if(jobTypes.size() < 1){
+            throw new HttpException(404,"Not Found");
+        }
+        for(JobType j : jobTypes){
+            jobTypeDTOS.add(JobTypeMapping.jobTypeDTO(j));
+        }
+        return jobTypeDTOS;
+    }
+
+    @Override
+    public JobTypeDTO Create(RequestJobType requestJobType) {
+        JobType jobType = JobTypeMapping.JobType(requestJobType);
+        _jobTypeRepo.save(jobType);
+        return (JobTypeDTO) JobTypeMapping.jobTypeDTO(jobType);
+    }
+
+    @Override
+    public JobTypeDTO Put(int id, RequestJobType requestJobType) {
+        JobType getJobType = _jobTypeRepo.findIdJobType(id);
+        JobType jobType = JobTypeMapping.JobTypePut(requestJobType,getJobType);
+        jobType.setId(id);
+        _jobTypeRepo.save(jobType);
+        return (JobTypeDTO) JobTypeMapping.jobTypeDTO(jobType);
+    }
+
+    @Override
+    public JobTypeDTO Delete(int id) {
+        _jobTypeRepo.deleteById(id);
+        return null;
+    }
+
+    @Override
+    public JobTypeDTO GetById(int id) {
+        JobType jobType = _jobTypeRepo.findIdJobType(id);
+        JobTypeDTO jobTypeDTO = JobTypeMapping.jobTypeDTO(jobType);
+        return jobTypeDTO;
+    }
+}
