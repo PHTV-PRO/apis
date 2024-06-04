660b25668f819fa1b62d6a62f23c396c0681ecad
phuslee <115574308+phuslee24@users.noreply.github.com>
2024-06-04 20:43:32 +0700

    up

--- src/main/java/com/company/phtv/Utils/Variable.java
+++ src/main/java/com/company/phtv/Utils/Variable.java
@@ -4,6 +4,8 @@ public interface Variable {
   public static HttpException notFound =  new HttpException(404,"not Found");
   public static HttpException tokenError =  new HttpException(400,"understand token request from client");
   public static HttpException emailOrPasswordIncorrect =  new HttpException(400, "Email or Password incorrect!!");
+  public static HttpException emailExisting =  new HttpException(400, "Email existing in database. please change email!!");
+
 
 
 
