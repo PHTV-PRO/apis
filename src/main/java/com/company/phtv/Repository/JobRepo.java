c2471ada438c7a7c2230c9654d75bf688b3c8446
Dustin9x <phamhuyhoang9x@gmail.com>
2024-06-30 20:30:52 +0700

    Update JobRepo.java

--- src/main/java/com/company/phtv/Repository/JobRepo.java
+++ src/main/java/com/company/phtv/Repository/JobRepo.java
@@ -13,9 +13,9 @@ import com.company.phtv.Models.Entity.FollowJob;
 
 public interface JobRepo extends JpaRepository<Jobs,Integer> {
 
-    @Query("SELECT j FROM Jobs j WHERE (:lotId is null or j.location.id = :lotId ) and (:indId is null or j.jobType.id = :indId ) ORDER BY j.start_date")
+    @Query("SELECT j FROM Jobs j WHERE (:lotId is null or j.location.id = :lotId ) and (:indId is null or j.jobType.id = :indId ) ORDER BY j.start_date DESC")
     List<Jobs> getAllJob(@Param("lotId") Long lotId, @Param("indId") Long indId);
-    @Query("select j from Jobs j Where  j.id = ?1 ORDER BY j.created_at")
+    @Query("select j from Jobs j Where  j.id = ?1 ORDER BY j.created_at DESC")
     Jobs findJobId(int id);
     @Query("SELECT j FROM Jobs j WHERE j.start_date < :currentDate ORDER BY j.start_date DESC")
     List<Jobs> findAllByStartDateBefore(@Param("currentDate") Date currentDate);
