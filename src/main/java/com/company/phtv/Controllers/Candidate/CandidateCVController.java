6092dc486ab203a5b433cae0f8b25998c7654a37
phuslee <115574308+phuslee24@users.noreply.github.com>
2024-07-16 23:29:36 +0700

    up fix post CV

--- src/main/java/com/company/phtv/Controllers/Candidate/CandidateCVController.java
+++ src/main/java/com/company/phtv/Controllers/Candidate/CandidateCVController.java
@@ -6,9 +6,11 @@ import com.company.phtv.Models.Request.RequestCV;
 import com.company.phtv.Services.CVService;
 import com.company.phtv.Utils.HttpException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -31,11 +33,11 @@ public class CandidateCVController {
         }
     }
 
-    @PostMapping(value = "", consumes = { MediaType.MULTIPART_FORM_DATA_VALUE }, produces = {
-            MediaType.APPLICATION_JSON_VALUE })
-    public ResponseEntity<?> post(@RequestBody RequestCV requestCV) {
+    @PostMapping( consumes = { MediaType.MULTIPART_FORM_DATA_VALUE }, produces = {
+        MediaType.APPLICATION_JSON_VALUE })
+    public ResponseEntity<?> post(@RequestParam MultipartFile file,@RequestParam String name ) {
         try {
-            return _baseController.success(_cvService.create(requestCV));
+            return _baseController.success(_cvService.create(new RequestCV( file, name)));
         } catch (HttpException e) {
             return _baseControllers.error(null, e.StatusCode, e.message);
         } catch (Exception e) {
